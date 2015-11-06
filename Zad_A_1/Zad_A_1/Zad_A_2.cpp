f80b753c42229b35ee348b69230135e4e66c88af
heyaheya <heyaheya@wp.pl>
2015-11-06 15:19:12 +0100

    aktualizacja

---
+++ Zad_A_1/Zad_A_1/Zad_A_2.cpp
@@ -0,0 +1,32 @@
+// Zad_A_1.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+//#include <ctype.h.>
+
+
+
+int main(int argc, char *argv[])
+
+{
+	int a, b;
+	double x, y;
+	float w;
+
+	printf("liczy funkcje ax+b=0\n");
+	printf("Podaj liczbe a i b - oddzielone spacjami:");
+
+	scanf("%i %i", &a, &b);
+	printf("\n");
+
+	x = a;
+	y = b;
+	w = -y / x;
+	printf("Wartosc szukana x=%.2f", w);
+
+	return 0;
+}
+
+
